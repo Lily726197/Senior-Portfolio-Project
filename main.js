cabbbfddd333b1cf0139093d13b69227ee971d68
Lily726197 <142992977+Lily726197@users.noreply.github.com>
2024-10-31 13:35:06 -0400

    dev-photo

---
+++ main.js
@@ -0,0 +1,4 @@
+$("#dev-photo").click(function()){
+    $("#dev-photo").animate({height: "600px", width: "500px"}, "slow");
+    $("#dev-photo").animate({height: "300px", width: "250px"}, "slow");
+};
