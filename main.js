5e640da1e4dc95960f5c2be9a79628547a8f5c5b
Lily726197 <142992977+Lily726197@users.noreply.github.com>
2024-12-03 13:42:35 -0500

    index finish

---
+++ main.js
@@ -0,0 +1,4 @@
+$("#dev-photo").click(function(){
+    $("#dev-photo").animate({height: "400px", width: "200px"}, "slow");
+    $("#dev-photo").animate({height: "200px", width: "100px"}, "slow");
+})
