6139c64c6ee4950f86ec93b37eafae37fb02ba9f
Lily726197 <142992977+Lily726197@users.noreply.github.com>
2024-12-04 13:38:05 -0500

    bpa logo

--- main.js
+++ main.js
@@ -2,3 +2,8 @@ $("#dev-photo").click(function(){
     $("#dev-photo").animate({height: "400px", width: "200px"}, "slow");
     $("#dev-photo").animate({height: "200px", width: "100px"}, "slow");
 })
+
+$("#bpa-logo").click(function(){
+    $("#bpa-logo").hide("slow");
+    $("#bpa-logo").show("slow");
+});
